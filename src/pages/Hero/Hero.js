acf8a1e61aa022ecdf8d64fab84ea2e5da13b412
Sheikh sadi <you@example.com>
2022-10-26 02:24:55 +0600

    hero at landing page added

--- src/pages/Hero/Hero.js
+++ src/pages/Hero/Hero.js
@@ -1,23 +1,20 @@
 import React from 'react';
+import image1 from '../../images/pngwing.com.png'
 
 const Hero = () => {
      return (
-          <section className="bg-gray-100 text-gray-800">
-               <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-                    <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-                         <h1 className="text-5xl font-bold leading-none sm:text-6xl">Ac mattis
-                              <span className="text-teal-600">senectus</span>erat pharetra
+          <section className="bg-gray-100 text-gray-800 max-w-[1370px] mx-auto">
+               <div className=" container flex flex-col justify-center p-2 mx-auto sm:py-10 lg:py-20 lg:flex-row lg:justify-between">
+                    <div className="  flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-lg  xl:max-w-lg lg:text-left">
+                         <h1 className="text-4xl font-bold leading-none sm:text-6xl">Learn from the best at<br /><span className="text-teal-600"> Course Arena</span>
                          </h1>
-                         <p className="mt-6 mb-8 text-lg sm:mb-12">Dictum aliquam porta in condimentum ac integer
-                              <br className="hidden md:inline lg:hidden" />turpis pulvinar, est scelerisque ligula sem
+                         <p className="mt-6 mb-8 text-lg sm:mb-12"> Start or upgrade yourself with 100+ course from the best teachers.
+                              <br className="hidden md:inline lg:hidden" />You will also get degree and certificate for accelerate your journey.
                          </p>
-                         <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                              <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded bg-teal-600 text-gray-50">Suspendisse</a>
-                              <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded border-gray-800">Malesuada</a>
-                         </div>
+                         
                     </div>
-                    <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-                         <img src="assets/svg/Business_SVG.svg" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
+                    <div className=" flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                         <img src={image1} alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
                     </div>
                </div>
           </section>
