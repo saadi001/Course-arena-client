cc4745c739f6bd3a4cc4bc3dbc98f864c456d6b1
Sheikh sadi <you@example.com>
2022-10-27 01:07:02 +0600

    authprovider added

--- src/pages/forms/Login.js
+++ src/pages/forms/Login.js
@@ -2,13 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import googlePng from '../../images/google.png';
 import githubPng from '../../images/github.png';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthProvider/Authprovider';
+import { useState } from 'react';
 
 
 
 const Login = () => {
+     const [error, setError] = useState('');
+     const {signIn} = useContext(AuthContext);
+
+     const handleForm = (e) =>{
+          e.preventDefault();
+          const form = e.target;
+          const email = form.email.value;
+          const password = form.password.value;
+          // console.log(email, password)
+
+          signIn(email,password)
+          .then(res =>{
+               const user = res.user;
+               console.log(user);
+               form.reset();
+               setError('')
+          })
+          .catch(error => {
+               console.error(error)
+               setError(error.message)
+          })
+     }
      return (
           <div>
-               <form className="w-full max-w-md mx-auto border p-4 mt-5 rounded shadow-md">
+               <form onSubmit={handleForm} className="w-full max-w-md mx-auto border p-4 mt-5 rounded shadow-md">
                     <h2 className='text-3xl text-center font-semibold'>Login</h2>
 
                     <div>
@@ -23,8 +48,12 @@ const Login = () => {
                          </label>
                          <input type="password" name="password" className=" px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="password" required />
                     </div>
+                    {/* error */}
+                    <p className='text-red-600 text-xs'>{error}</p>
+
                     <p className='py-1 text-sm text-slate-600 '>New here? <Link className='underline' 
                     to='/register'>create acount</Link></p>
+
                     <div className='flex justify-between mt-3 '>
                          <div><button type='submit' className='py-2 px-3 rounded bg-teal-500'>Login</button></div>
                          <div className='flex'>
--- src/pages/forms/Register.js
+++ src/pages/forms/Register.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import googlePng from '../../images/google.png';
 import githubPng from '../../images/github.png';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthProvider/Authprovider';
 
 const Register = () => {
+     const [error, setError] = useState('');
+
+     const {createUser,updateUserProfile} = useContext(AuthContext);
+
+     const handleForm = (e) =>{
+          e.preventDefault();
+          const form = e.target;
+          const name = form.name.value;
+          const url = form.url.value;
+          const email = form.email.value;
+          const password = form.password.value;
+
+          createUser(email, password)
+          .then(res =>{
+               const user = res.user;
+               console.log(user);
+               form.reset();
+               setError('');
+
+          })
+          .catch(e=>{
+               console.error(e);
+               setError(e.message)
+          })
+          
+     }
      return (
           <div className=''>
-               <form className="w-full max-w-md mx-auto border p-4 mt-5 rounded shadow-md">
+               <form onSubmit={handleForm} className="w-full max-w-md mx-auto border p-4 mt-5 rounded shadow-md">
                     <h2 className='text-3xl text-center font-semibold'>Register</h2>
                     <div>
                          <label className="label">
@@ -32,6 +60,7 @@ const Register = () => {
                          </label>
                          <input type="password" name="password" className=" px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="password" required />
                     </div>
+                    {<p className='text-red-600 text-xs py-1'>{error}</p>}
                     <p className='py-1 text-sm text-slate-600 '>Aleady have account? <Link className='underline' to='/login'>login</Link></p>                  
                     <div className='flex justify-between mt-3 '>
                          <div><button type='submit' className='py-2 px-3 rounded bg-teal-500'>Register</button></div>
