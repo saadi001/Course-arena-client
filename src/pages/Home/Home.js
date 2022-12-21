922b797f27baaf8dbee4aeccd1fe29ced38955a3
Sheikh sadi <sheikhsadi1620@gmail.com>
2022-12-22 03:18:10 +0600

    footer added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
 import Hero from '../Hero/Hero';
 
 const Home = () => {
      return (
           <div>
                <Hero></Hero>
+               <Footer></Footer>
           </div>
      );
 };
